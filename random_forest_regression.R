b15dd18db8a3f74a357e7450cce6e87a91f72493
Juan Manuel de Palacios <juanma36@gmail.com>
2020-02-05 15:48:33 -0300

    Regresión de bosque aletorio

--- random_forest_regression.R
+++ random_forest_regression.R
@@ -1,41 +1,48 @@
-# Random Forest Regression
+# Regresión con bosques aleatorios 
 
-# Importing the dataset
+#Importar el dataset
 dataset = read.csv('Position_Salaries.csv')
-dataset = dataset[2:3]
+dataset = dataset[, 2:3]
 
-# Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
+
+#Dividir los datos en conjunto de entrenamiento y conjunto de test
+#install.packages("caTools")
 # library(caTools)
 # set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 # training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
+# testing_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+#Escalado de valores
+# training_set[,2:3] = scale(training_set[,2:3])
+# testing_set[,2:3] = scale(testing_set[,2:3])
 
-# Fitting Random Forest Regression to the dataset
-# install.packages('randomForest')
+# AJustar Modelo de Random Forest con el Conjunto de Datos
+#install.packages("randomForest")
 library(randomForest)
 set.seed(1234)
-regressor = randomForest(x = dataset[-2],
-                         y = dataset$Salary,
-                         ntree = 500)
+regression = randomForest(x = dataset[1],
+                          y = dataset$Salary,
+                          ntree = 500)
 
-# Predicting a new result with Random Forest Regression
-y_pred = predict(regressor, data.frame(Level = 6.5))
+#Predicción de nuevos resultadosde la Regresión
+y_pred = predict(regression, newdata = data.frame(Level = 6.5))
 
-# Visualising the Random Forest Regression results (higher resolution)
-# install.packages('ggplot2')
+
+# Visualización del modelo de regresión
+# install.packages("ggplot2")
 library(ggplot2)
 x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Random Forest Regression)') +
-  xlab('Level') +
-  ylab('Salary')
+             color = "red") +
+  geom_line(aes(x = x_grid, y = predict(regression, 
+                                        newdata = data.frame(Level = x_grid))),
+            color = "blue") +
+  ggtitle("Predicción (Random Forest") + 
+  xlab("Nivel del empleado") +
+  ylab("Sueldo (en $)")
+
+
+
+
